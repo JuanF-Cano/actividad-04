d545870eb2d3d39613acd780017cce1fd4688be4
AGBR121 <angel.burbano@utp.edu.co>
2025-03-10 06:56:12 -0500

    agrego hechos y reglas del punto 1.3

--- actividad4.pl
+++ actividad4.pl
@@ -1,6 +1,25 @@
+mujer(clara).
+mujer(isabel).
+mujer(ana).
+mujer(patricia).
+hombre(tomas).
+hombre(jose).
+hombre(jaime).
+
 progenitor(clara, jose).
 progenitor(tomas, jose).
 progenitor(tomas, isabel).
 progenitor(jose, ana).
 progenitor(jose, patricia).
 progenitor(patricia, jaime).
+
+dif(X,Y) :- X \= Y.
+
+es_madre(X) :- mujer(X), progenitor(X,_).
+es_padre(X) :- hombre(X), progenitor(X,_).
+es_hijo(X) :- progenitor(_,X).
+hermana_de(X,Y) :- mujer(X), progenitor(Z,X), progenitor(Z,Y), dif(X,Y).
+abuelo_de(X,Y) :- hombre(X), progenitor(X,Z), progenitor(Z,Y), dif(X,Y).
+abuela_de(X,Y) :- mujer(X), progenitor(X,Z), progenitor(Z,Y), dif(X,Y).
+hermanos(X,Y) :- progenitor(Z,X), progenitor(Z,Y), dif(X,Y).
+tia(X,Y) :- mujer(X), progenitor(Z,Y), hermana_de(X,Z).
